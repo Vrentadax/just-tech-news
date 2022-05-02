2c100f2f9297502ba777fd85949b0901f07397f7
Vrentadax <vrentadax@outlook.com>
2022-05-02 15:47:22 -0600

    finished auth

--- server.js
+++ server.js
@@ -3,10 +3,25 @@ const express = require('express');
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const exphbs = require('express-handlebars');
+const session = require('express-session');
 const hbs = exphbs.create({});
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+  secret: process.env.cookie,
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+app.use(session(sess));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
